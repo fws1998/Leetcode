be82490d98743e76544a23c8091f1074ef641843
charlie <404749592@qq.com>
2021-07-29 15:12:15 +1000

    ques 20

--- src/Solution20.java
+++ src/Solution20.java
@@ -1,7 +1,7 @@
 import java.util.EmptyStackException;
 import java.util.Stack;
 
-class Solution {
+class Solution20 {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
         for (short i = 0; i<s.length(); i++){
@@ -21,6 +21,6 @@ class Solution {
     }
 
     public static void main(String[] args) {
-        boolean is = new Solution().isValid("()");
+        boolean is = new Solution20().isValid("()");
     }
 }
